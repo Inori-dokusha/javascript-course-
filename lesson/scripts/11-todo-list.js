daa0365e37400cad2ed6add33d9f62ae821e7975
Inori-dokusha <inori.dokusha@gmail.com>
2024-05-26 10:48:06 +0700

    update

--- lesson/scripts/11-todo-list.js
+++ lesson/scripts/11-todo-list.js
@@ -1,4 +1,10 @@
-const todoList = [];
+const todoList = [{
+	name: 'make dinner',
+	dueDate: '2022-12-22'
+}, {
+	name: 'wash dishes',
+	dueDate: '2022-12-22'
+}];
 
 renderTodoList();
 
@@ -6,14 +12,17 @@ function renderTodoList() {
 	let todoListHtml = '';
 
 	for (let i = 0; i < todoList.length; i++) {
-		const todo = todoList[i];
+		const todoObject = todoList[i];
+		//const name = todoObject.name;
+		//const dueDate = todoObject.dueDate;
+		const { name, dueDate } = todoObject;
 		const html = `
-		<p>
-			${todo}
-			<button onclick="
-				todoList.splice(${i}, 1);
-			">Delete</button>
-		</p>`;
+		<div>${name}</div>
+		<div>${dueDate}</div>
+		<button onclick="
+			todoList.splice(${i}, 1);
+		" class="delete-todo-button">Delete</button>
+		`;
 		todoListHtml += html;
 	}
 
@@ -24,7 +33,15 @@ function addTodo() {
 	const inputElement = document.querySelector('.js-name-input');
 	const name = inputElement.value;
 
-	todoList.push(name);
+	const dateInputElement = document.querySelector('.js-due-date-input');
+	const dueDate = dateInputElement.value;
+
+	todoList.push({
+		//name: name,
+		//dueDate: dueDate,
+		name,
+		dueDate
+	});
 	console.log(todoList);
 
 	inputElement.value = '';
